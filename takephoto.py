67566983a299da794bd9df325bbc244ddf5d9900
Isaac <isaacmactan@gmail.com>
2021-04-29 17:53:23 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -13,13 +13,13 @@ try:
     while True:
     	button_state = GPIO.input(23)
     	if button_state == True:
-    		print "Open!"
+    		print "Closed!"
     		# camera.start_preview()
     		# sleep(5)
     		# camera.capture('/home/pi/Desktop/image1.jpg')
     		# camera.stop_preview()
     		# time.sleep(0.2)
     	else:
-    		print "Closed!"
+    		print "Open!"
 except:
 	GPIO.cleanup()
