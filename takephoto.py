571797369c055851e659d7d67516714b5737e4d8
Isaac <isaacmactan@gmail.com>
2021-04-29 16:59:58 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -1,34 +1,20 @@
 from picamera import PiCamera
 from time import sleep
-# import gpiozero
-# import RPi.GPIO as GPIO
-import wiringpi2 as wiringpi
+import gpiozero
+import RPi.GPIO as GPIO
 
-# initialize
-wiringpi.wiringPiSetup()
-
-GPIO24 = 5
-LOW = 0
-INPUT = 0
-PULL_DOWN = 1
-wiringpi.pinMode(GPIO24, INPUT)  # push button
-wiringpi.pullUpDnControl(GPIO24, PULL_DOWN)  # pull down
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
 
 
 camera = PiCamera()
 
 try:
-    clear_all()
-    while 1:
-        button_state = wiringpi.digitalRead(GPIO24)
-        print button_state
-        if button_state == 1:
-            camera.start_preview()
-            sleep(5)
-            camera.capture('/home/pi/Desktop/image1.jpg')
-            camera.stop_preview()
-        wiringpi.delay(20)
-except KeyboardInterrupt:
-    clear_all()
-
-print("done")
+    while True:
+    	button_state = GPIO.input(23)
+    	if button_state == False:
+    		GPIO.output(24, True)
+    		print('Button Pressed...')
+    		time.sleep(0.2)
+except:
+	GPIO.cleanup()
