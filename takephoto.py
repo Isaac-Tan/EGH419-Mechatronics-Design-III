a878f3896b4bdf4e9a0e79e7c23e2e5ca999b76a
Isaac <isaacmactan@gmail.com>
2021-04-29 17:53:05 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -4,7 +4,7 @@ import gpiozero
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
+GPIO.setup(23, GPIO.IN)#Button to GPIO23
 
 
 camera = PiCamera()
