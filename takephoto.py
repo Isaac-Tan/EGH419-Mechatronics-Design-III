aad736755e2a13703f5c69023888617b8b9e5271
Isaac <isaacmactan@gmail.com>
2021-04-29 17:19:56 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -13,12 +13,13 @@ try:
     while True:
     	button_state = GPIO.input(23)
     	if button_state == False:
+    		print "Hello, World!"
+    		
+    	else:
     		camera.start_preview()
     		sleep(5)
     		camera.capture('/home/pi/Desktop/image1.jpg')
     		camera.stop_preview()
     		time.sleep(0.2)
-    	else:
-    		print "Hello, World!"
 except:
 	GPIO.cleanup()
