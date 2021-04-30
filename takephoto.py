f65c11b6cbceba64b1f163c0273ec13dd795517e
Isaac <isaacmactan@gmail.com>
2021-04-30 15:16:38 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -2,12 +2,21 @@ from picamera import PiCamera
 from time import sleep
 import gpiozero
 import RPi.GPIO as GPIO
+from dateutil.parser import parse as dp
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)#Button to GPIO23
 
 import datetime
 
+def timestamp():
+	t = datetime.datetime.now()
+	t_iso = t.isoformat()
+	t_parsed = dp(t_iso)
+	t_secs = t_parsed.strftime('%s')
+	print (t_secs)
+	return t_secs
 
 
 camera = PiCamera()
@@ -17,11 +26,12 @@ try:
     	button_state = GPIO.input(23)
     	if button_state == True:
     		print "Closed!"
-    		# camera.start_preview()
-    		# sleep(5)
-    		# camera.capture('/home/pi/Desktop/image1.jpg')
-    		# camera.stop_preview()
-    		# time.sleep(0.2)
+    		camera.start_preview()
+    		sleep(5)
+    		filename = timestamp()
+    		camera.capture('/home/pi/Photos/%s.jpg' filename)
+    		camera.stop_preview()
+    		time.sleep(0.2)
     	else:
     		print "Open!"
 except:
