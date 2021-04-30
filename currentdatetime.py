daad99b9012fefa38f85e7b01cdce1b156fd8ffe
Isaac <isaacmactan@gmail.com>
2021-04-30 12:48:00 +1000

    Update currentdatetime.py

--- currentdatetime.py
+++ currentdatetime.py
@@ -5,11 +5,8 @@ from datetime import datetime
 from dateutil.parser import parser
 
 
-try:
-	while True:
-		t = datetime.datetime.now()
-		t_secs = parse(t).timestamp()
-		print ("Current date & time = %s" % t_secs)
-		sleep(5)
-except:
-	print ("Finish")
+while True:
+	t = datetime.datetime.now()
+	t_secs = parse(t).timestamp()
+	print ("Current date & time = %s" % t_secs)
+	sleep(5)
