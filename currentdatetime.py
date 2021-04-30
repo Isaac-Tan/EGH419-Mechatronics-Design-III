41a0bf5b91e77e79ad201cb29a9a319a8ae4a0cb
Isaac <isaacmactan@gmail.com>
2021-04-30 13:01:45 +1000

    Update currentdatetime.py

--- currentdatetime.py
+++ currentdatetime.py
@@ -6,7 +6,7 @@ from dateutil.parser import parse as dp
 while True:
 	t = datetime.datetime.now()
 	t_iso = t.isoformat()
-	t_parsed = dp.parse(t_iso)
+	t_parsed = dp(t_iso)
 	t_secs = t_parsed.strftime('%s')
 	print ("Current date & time = %s" % t_secs)
 	sleep(5)
