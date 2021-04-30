dc17ec3ecad739993934d6c34e6ff4755650dcdc
Isaac <isaacmactan@gmail.com>
2021-04-30 12:20:24 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -6,14 +6,20 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)#Button to GPIO23
 
+import datetime
+
+
 
 camera = PiCamera()
 
 try:
     while True:
     	button_state = GPIO.input(23)
-    	if button_state == True:
+    	# if button_state == True:
+    	if button_state == False:
     		print "Closed!"
+    		i = datetime.datetime.now()
+    		print ("Current date & time = %s" % i)
     		# camera.start_preview()
     		# sleep(5)
     		# camera.capture('/home/pi/Desktop/image1.jpg')
