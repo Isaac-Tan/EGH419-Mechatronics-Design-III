05cf8e9306c5cafc9cae49aef496159d60cd340d
Isaac <isaacmactan@gmail.com>
2021-05-05 18:33:12 +1000

    Update currentdatetime.py

--- currentdatetime.py
+++ currentdatetime.py
@@ -2,11 +2,14 @@ import datetime
 from time import sleep
 from dateutil.parser import parse as dp
 
-
-while True:
+def timestamp():
 	t = datetime.datetime.now()
 	t_iso = t.isoformat()
 	t_parsed = dp(t_iso)
 	t_secs = t_parsed.strftime('%s')
 	print (t_secs)
-	sleep(5)
+	return t_secs
+
+while True:
+	filename = timestamp()
+    print("This is the filename: %s", filename)
