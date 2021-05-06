3bc87bd9604440aa7302a900ecf4e957b45cee96
Isaac <isaacmactan@gmail.com>
2021-05-06 15:46:03 +1000

    Update turnonled.py

--- turnonled.py
+++ turnonled.py
@@ -7,6 +7,7 @@ GPIO.setup(23, GPIO.IN)#Button1 to GPIO23
 GPIO.setup(24, GPIO.OUT)#LED to GPIO24
 GPIO.setup(25, GPIO.IN)#Button2 to GPIO25
 
-GPIO.output(24, 1)
+while True:
+	GPIO.output(24,1)
 
 GPIO.cleanup()
