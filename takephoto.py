0dbf358f334caafc4de0f1f36237bca9f97221ab
Isaac <isaacmactan@gmail.com>
2021-05-06 12:08:56 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -24,35 +24,32 @@ def timestamp():
 
 camera = PiCamera()
 
-try:
-    while True:
-    	button1_state = GPIO.input(23)
-    	if button1_state == True:
-    		print "Closed!"
-    		camera.start_preview()
-    		sleep(1)
-    		filename = str(timestamp())
-    		extension = ".jpg"
-    		camera.capture('/home/pi/Photos/'+ filename + extension)
-    		
-    		GPIO.output(24, 1)
-    		sleep(5)
-    		camera.stop_preview()
-    		time.sleep(0.2)
-    	else:
-    		print "Open!"
-    		GPIO.output(24,0)
-    	# button2_state = GPIO.input(25)
-    	# if button2_state == True:
-    	# 	print "Closed!"
-    	# 	camera.start_preview()
-    	# 	sleep(5)
-    	# 	filename = timestamp()
-    	# 	camera.capture('/home/pi/Photos/%s.jpg' filename)
-    	# 	camera.stop_preview()
-    	# 	time.sleep(0.2)
-    	# else:
-    	# 	print "Open!"
 
-except:
-	GPIO.cleanup()
+while True:
+	button1_state = GPIO.input(23)
+	if button1_state == True:
+		print "Closed!"
+		camera.start_preview()
+		sleep(1)
+		filename = str(timestamp())
+		extension = ".jpg"
+		camera.capture('/home/pi/Photos/'+ filename + extension)
+		
+		GPIO.output(24, 1)
+		sleep(5)
+		camera.stop_preview()
+		time.sleep(0.2)
+	else:
+		print "Open!"
+		GPIO.output(24,0)
+	# button2_state = GPIO.input(25)
+	# if button2_state == True:
+	# 	print "Closed!"
+	# 	camera.start_preview()
+	# 	sleep(5)
+	# 	filename = timestamp()
+	# 	camera.capture('/home/pi/Photos/%s.jpg' filename)
+	# 	camera.stop_preview()
+	# 	time.sleep(0.2)
+	# else:
+	# 	print "Open!"
