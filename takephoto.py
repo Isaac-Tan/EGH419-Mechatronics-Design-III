b8aa99e9b88ef28bc053936d9f807e6a3f0750c0
Isaac <isaacmactan@gmail.com>
2021-05-19 13:51:12 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -1,20 +1,11 @@
 from picamera import PiCamera
 from time import sleep
-import gpiozero
-import RPi.GPIO as GPIO
 from dateutil.parser import parse as dp
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)#Button1 to GPIO23
-GPIO.setup(16, GPIO.OUT)#LED to GPIO16
-GPIO.setup(25, GPIO.IN)#Button2 to GPIO25
 
 
 import datetime
 sessionActive = False
 camera = PiCamera()
-GPIO.output(16,1)
 
 def timestamp():
 	t = datetime.datetime.now()
@@ -42,32 +33,18 @@ def takephoto(case):
 
 def startsession():
 	print "Starting Session!"
-	sessionActive = True
 	sleep(1)
 	takephoto("pre")
-	GPIO.output(16, 0)
 	sleep(5)
 
 def endsession():
 	print "Ending Session!"
-	sessionActive = False
 	sleep(1)
 	print "Please return the equipment to the locker and close the door."
 	sleep(3)
 	takephoto("post")
-	GPIO.output(16, 1)
 	sleep(5)
 
 
 
-while True:
-	button1_state = GPIO.input(23)
-	if button1_state == True:
-		startsession()
-	else:
-		print ""
-	button2_state = GPIO.input(25)
-	if button2_state == True:
-		endsession()
-	else:
-		print ""
+startsession()
