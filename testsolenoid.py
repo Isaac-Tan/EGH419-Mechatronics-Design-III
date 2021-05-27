d7ce0de449aa61bb7e8bc24fdb4ab1dcd14c4f29
Isaac <isaacmactan@gmail.com>
2021-05-27 23:22:49 +1000

    Update testsolenoid.py

--- testsolenoid.py
+++ testsolenoid.py
@@ -5,8 +5,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)#LED to GPIO24
 
+now = time.time()	#start process time
 GPIO.output(25, 1)
 sleep(0.05)
 GPIO.output(25, 0)
+elapsed = time.time() - now			#end process time
+print("Process: Open Door")
+print("Latency: ", elapsed)
+
 
 GPIO.cleanup()
