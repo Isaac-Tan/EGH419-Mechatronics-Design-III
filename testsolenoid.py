f89123979e69482cbd20da1cb73c8897fee3694b
Isaac <isaacmactan@gmail.com>
2021-05-27 14:05:50 +1000

    Update testsolenoid.py

--- testsolenoid.py
+++ testsolenoid.py
@@ -3,12 +3,10 @@ import gpiozero
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)#Button1 to GPIO23
-GPIO.setup(24, GPIO.OUT)#LED to GPIO24
-GPIO.setup(25, GPIO.IN)#Button2 to GPIO25
+GPIO.setup(25, GPIO.OUT)#LED to GPIO24
 
-GPIO.output(24, 1)
+GPIO.output(25, 1)
 sleep(1)
-GPIO.output(24, 0)
+GPIO.output(25, 0)
 
 GPIO.cleanup()
