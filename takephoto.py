dcf104633771f66d0deeebe22416eecee9545b16
Isaac <isaacmactan@gmail.com>
2021-05-28 12:55:35 +1000

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -2,6 +2,12 @@ from picamera import PiCamera
 import time
 from time import sleep
 from dateutil.parser import parse as dp
+import gpiozero
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)#LED to GPIO24
+
 
 
 import datetime
@@ -15,13 +21,18 @@ def timestamp():
 	return t_secs
 
 def takephoto():
+	GPIO.output(16, 1)						#Sets the relay signal for the LED high
 	extension = ".jpg"
 	ts = timestamp()
 	filename = str(ts)
 	camera.capture('/home/pi/Photos/Testing/' + filename + extension)
 	print "Image taken."
+	GPIO.output(16, 0)						#Sets the relay signal for the LED low
+
+
 now = time.time()
 takephoto()
 elapsed = time.time() - now
 print("Process: Take Photo")
 print("Latency: ", elapsed)
+GPIO.cleanup()
