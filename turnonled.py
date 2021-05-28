9af531e2c4c36d8b627064db1fba092cf3552b7b
Isaac <isaacmactan@gmail.com>
2021-05-28 12:33:37 +1000

    Update turnonled.py

--- turnonled.py
+++ turnonled.py
@@ -3,12 +3,12 @@ import gpiozero
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT)#LED to GPIO24
+GPIO.setup(16, GPIO.OUT)#LED to GPIO24
 GPIO.setwarnings(False)
 
 while True:
-	GPIO.output(24,GPIO.HIGH)
+	GPIO.output(16,GPIO.HIGH)
 	sleep(2)
-	GPIO.output(24,GPIO.LOW)
+	GPIO.output(16,GPIO.LOW)
 
 GPIO.cleanup()
