1dc1c1ee9cea452e410496f4db8ee469a494a565
Isaac <isaacmactan@gmail.com>
2021-05-28 12:46:44 +1000

    Update gpioclean.py

--- gpioclean.py
+++ gpioclean.py
@@ -5,8 +5,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)#Button1 to GPIO23
 GPIO.setup(24, GPIO.OUT)#LED to GPIO24
+GPIO.setup(16, GPIO.OUT)#LED to GPIO16
 GPIO.setup(25, GPIO.IN)#Button2 to GPIO25
 
 GPIO.output(24, 0)
+GPIO.output(16, 0)
 
 GPIO.cleanup()
