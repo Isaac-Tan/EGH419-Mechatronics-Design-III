576a9306880648edfc04b13805b4af0b67ffa265
Isaac <isaacmactan@gmail.com>
2021-05-31 20:17:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def action(deviceName, action):
 	}
 	return render_template('checkin.html', **templateData)
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+   app.run(host='0.0.0.0', port=80, debug=False)
